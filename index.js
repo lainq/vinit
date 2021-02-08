b40f42a71a8d8003bc693ecefd46b1a1d43b1956
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-08 16:21:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ let score = 0
 const text = document.querySelector('.ans')
 
 
+function changeInputTextValue(textbox, newValue){
+    if(newValue == null){
+        alert("An error occured")
+    } else {
+        textbox.value = newValue
+    }
+}
+
+function getIInputTextBoxText(textBox){
+    return textBox.value
+}
+
+function calculateEquation(equation){
+    if(equation == null){
+            return null
+    } else {
+        return eval(equation.toString().replace(" ", ""))
+    }
+}
+
+
 // the buttons
 const buttons = document.getElementsByTagName('button')
 
