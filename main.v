1adb9674d9532df96a657c89a18d192f4af188f9
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-15 16:41:11 +0530

    test.v

---
+++ main.v
@@ -0,0 +1,11 @@
+// Create and remove insufficent data strucrures
+// from the code ans optimize the data for
+// people to use as a good thing
+fn createDataLog(n int, y int) int {
+	for data := n; data < n+y; data++ {
+		if data == (y/(n+n)) {
+			break;
+		}
+	}
+
+}
