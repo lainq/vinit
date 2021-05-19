2833caf337991ccdff44eb0eb5bf954621ce391a
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-19 19:04:54 +0530

    Initialized scriptas

--- vinit/arguments/perform.v
+++ vinit/arguments/perform.v
@@ -1,9 +1,12 @@
 module arguments
 
-import commands { initialize_v_project }
+import commands { initialize_v_project, execute_v_script }
 
 pub fn execute_command(command CurrentCharacterType, parameters map[string]string) {
 	if (command as string) == "arguments.CurrentCharacterType('init')" {
 		initialize_v_project()
+	} else {
+		execute_v_script(command as string, parameters)
 	}
+
 }
---
+++ vinit/commands/scripts.v
@@ -0,0 +1,52 @@
+module commands
+
+import os { is_file, join_path, getwd, args, open, read_file } 
+import exception { VinitException }
+
+struct ScriptsFile {
+	filename string
+	root string
+mut:
+	exists bool
+}
+
+struct InvalidCommandException {
+	message string
+	suggestion string
+	fatal bool
+}
+
+fn (params InvalidCommandException) throw_exception() {
+	error := VinitException{
+		exception_message : params.message
+		exception_suggestion : params.suggestion,
+		fatal : params.fatal
+	}
+	error.raise()
+}
+
+fn find_script_file(script_file ScriptsFile) bool {
+	filepath := join_path(script_file.root, script_file.filename)
+	return is_file(filepath)
+}
+
+pub fn execute_v_script(script string, variables map[string]string) {
+	mut file := ScriptsFile{
+		filename : 'v.scripts',
+		root : getwd(),
+	}
+	file.exists = find_script_file(file)
+	if !file.exists {
+		InvalidCommandException{
+			message : '${args[1..][0]} is not a command',
+			fatal : true
+		}.throw_exception()
+	} else {
+		file_content := read_file(join_path(file.root, file.filename)) or {
+			panic(err)
+			return
+		}
+
+		println(file_content)
+	}
+}
