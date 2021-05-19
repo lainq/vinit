dd6fd1954e047f92f6ee292aba1cce8c750e87e2
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-19 21:18:13 +0530

    fmt: Formatted the code

--- vinit/commands/scripts.v
+++ vinit/commands/scripts.v
@@ -1,26 +1,36 @@
 module commands
 
-import os { is_file, join_path, getwd, args, open, read_file } 
+import os { args, getwd, is_file, join_path, read_file }
 import exception { VinitException }
+
+struct ScriptFileParser {
+	data []string
+}
+
+fn (parser ScriptFileParser) parse() {
+	for line in parser.data {
+		println(line)
+	}
+}
 
 struct ScriptsFile {
 	filename string
-	root string
+	root     string
 mut:
 	exists bool
 }
 
 struct InvalidCommandException {
-	message string
+	message    string
 	suggestion string
-	fatal bool
+	fatal      bool
 }
 
 fn (params InvalidCommandException) throw_exception() {
 	error := VinitException{
-		exception_message : params.message
-		exception_suggestion : params.suggestion,
-		fatal : params.fatal
+		exception_message: params.message
+		exception_suggestion: params.suggestion
+		fatal: params.fatal
 	}
 	error.raise()
 }
@@ -32,14 +42,14 @@ fn find_script_file(script_file ScriptsFile) bool {
 
 pub fn execute_v_script(script string, variables map[string]string) {
 	mut file := ScriptsFile{
-		filename : 'v.scripts',
-		root : getwd(),
+		filename: 'v.scripts'
+		root: getwd()
 	}
 	file.exists = find_script_file(file)
 	if !file.exists {
 		InvalidCommandException{
-			message : '${args[1..][0]} is not a command',
-			fatal : true
+			message: '${args[1..][0]} is not a command'
+			fatal: true
 		}.throw_exception()
 	} else {
 		file_content := read_file(join_path(file.root, file.filename)) or {
